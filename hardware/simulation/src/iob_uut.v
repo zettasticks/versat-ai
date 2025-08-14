af4d009332aad52a61c4b9de1e0d5de4c2051f14
PMiranda <pmiranda.iobundle@gmail.com>
2025-08-14 20:00:27 +0100

    update(py2): start py2 update - update to latest py2hwsw main commit     - update csrs configuration     - use different name from `interface` (system verilog keyword)     - replace `instatiator` with `issuer`     - remove `iob_rready` signals - `make pc-emul-run TEST=test1` working - but `make sim-run TEST=test1` hangs at the start of console/simulation - NOTE: sometimes, setup takes more time due to license download step

--- hardware/simulation/src/iob_uut.v
+++ hardware/simulation/src/iob_uut.v
@@ -21,8 +21,7 @@ module iob_uut #(
    input  [ 4-1:0] iob_wstrb_i,
    output          iob_rvalid_o,
    output [32-1:0] iob_rdata_o,
-   output          iob_ready_o,
-   input           iob_rready_i
+   output          iob_ready_o
 );
 
    // rs232 bus
@@ -147,8 +146,7 @@ module iob_uut #(
       .iob_csrs_iob_wstrb_i (iob_wstrb_i),
       .iob_csrs_iob_rvalid_o(iob_rvalid_o),
       .iob_csrs_iob_rdata_o (iob_rdata_o),
-      .iob_csrs_iob_ready_o (iob_ready_o),
-      .iob_csrs_iob_rready_i(iob_rready_i)
+      .iob_csrs_iob_ready_o (iob_ready_o)
    );
 
    // External memory
