097be07966e0670101c5cd771a840c8bdd92002f
PMiranda <pmiranda.iobundle@gmail.com>
2025-08-19 18:34:36 +0100

    update(py2): remove local iob_system copy - remove local iob_system copy - update versat_ai to use `parent` to make changes/overwrites to default   `iob_system` from py2hwsw

--- versat_ai.py
+++ versat_ai.py
@@ -5,6 +5,66 @@
 
 def setup(py_params_dict):
     # Py2hwsw dictionary describing current core
+    mem_addr_w = 22
+    name = "versat_ai"
+    addr_w = 32
+    data_w = 32
+
+    # setup custom xbar
+    xbar_subblock = {
+        "core_name": "iob_axi_full_xbar",
+        "name": f"{name}_axi_full_xbar",
+        "instance_name": "iob_axi_full_xbar",
+        "instance_description": "AXI full xbar instance",
+        "parameters": {
+            "ID_W": "AXI_ID_W",
+            "LEN_W": "AXI_LEN_W",
+        },
+        "connect": {
+            "clk_en_rst_s": "clk_en_rst_s",
+            "rst_i": "rst",
+            "s0_axi_s": "cpu_ibus",
+            "s1_axi_s": "cpu_dbus",
+            "s2_axi_s": "versat_axi",
+            # Manager interfaces connected below
+        },
+        "addr_w": addr_w,
+        "data_w": data_w,
+        "lock_w": 1,
+        "num_subordinates": 3,
+    }
+    xbar_manager_interfaces = {
+        "use_extmem": (
+            "axi_m",
+            [
+                "{unused_m1_araddr_bits, axi_araddr_o}",
+                "{unused_m1_awaddr_bits, axi_awaddr_o}",
+            ],
+        ),
+        "use_bootrom": (
+            "bootrom_cbus",
+            [
+                "{unused_m2_araddr_bits, bootrom_axi_araddr}",
+                "{unused_m2_awaddr_bits, bootrom_axi_awaddr}",
+            ],
+        ),
+        "use_peripherals": (
+            "axi_periphs_cbus",
+            [
+                "{unused_m3_araddr_bits, periphs_axi_araddr}",
+                "{unused_m3_awaddr_bits, periphs_axi_awaddr}",
+                "periphs_axi_awlock[0]",
+                "periphs_axi_arlock[0]",
+            ],
+        ),
+    }
+    # Connect xbar manager interfaces
+    num_managers = 0
+    for interface_connection in xbar_manager_interfaces.values():
+        xbar_subblock["connect"] |= {f"m{num_managers}_axi_m": interface_connection}
+        num_managers += 1
+    xbar_subblock["num_managers"] = num_managers
+
     core_dict = {
         "version": "0.8",
         "parent": {
@@ -13,6 +73,10 @@ def setup(py_params_dict):
             "core_name": "iob_system",
             # Every parameter in the lines below will be passed to the iob_system parent core.
             # Full list of parameters availabe here: https://github.com/IObundle/py2hwsw/blob/main/py2hwsw/lib/iob_system/iob_system.py
+            "use_intmem": False,
+            "use_extmem": True,
+            "mem_addr_w": mem_addr_w,
+            "include_tester": False,
             "cpu": "iob_vexriscv",
             # NOTE: Place other iob_system python parameters here
             "system_attributes": {
@@ -21,6 +85,26 @@ def setup(py_params_dict):
                     "iob_aes_ku040_db_g",
                     "iob_cyclonev_gt_dk",
                     "iob_zybo_z7",
+                ],
+                "title": "Versat-AI System",
+                "description": "Accelerate AI Applications with Versat-AI.",
+                "confs": [
+                    {  # Needed for software and makefiles
+                        "name": "MEM_ADDR_W",
+                        "descr": "External memory bus address width.",
+                        "type": "M",
+                        "val": mem_addr_w,
+                        "min": "0",
+                        "max": "32",
+                    },
+                    {
+                        "name": "EXT_MEM_HEXFILE",
+                        "descr": "Firmware file name",
+                        "type": "D",
+                        "val": f'"{name}_firmware"',
+                        "min": "NA",
+                        "max": "NA",
+                    },
                 ],
                 "ports": [
                     {
@@ -33,7 +117,23 @@ def setup(py_params_dict):
                     },
                     # NOTE: Add other ports here.
                 ],
+                "wires": [
+                    {
+                        "name": "versat_axi",
+                        "descr": "Versat axi wires",
+                        "signals": {
+                            "type": "axi",
+                            "prefix": "versat_",
+                            "ID_W": "AXI_ID_W",
+                            "ADDR_W": addr_w,
+                            "DATA_W": data_w,
+                            "LEN_W": "AXI_LEN_W",
+                            "LOCK_W": "1",
+                        },
+                    },
+                ],
                 "subblocks": [
+                    xbar_subblock,
                     {
                         # Instantiate a UART core from: https://github.com/IObundle/py2hwsw/tree/main/py2hwsw/lib/hardware/iob_uart
                         "core_name": "iob_uart",
@@ -58,6 +158,14 @@ def setup(py_params_dict):
                             "clk_en_rst_s": "clk_en_rst_s",
                             # Cbus connected automatically
                         },
+                    },
+                    {
+                        "core_name": "iob_versat",
+                        "instance_name": "VERSAT0",
+                        "instance_description": "Versat accelerator",
+                        "is_peripheral": True,
+                        "parameters": {},
+                        "connect": {"axi_out_m": "versat_axi"},
                     },
                     # NOTE: Add other components/peripherals here.
                 ],
