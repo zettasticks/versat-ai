dd2958fcfe962d2d49a10fcdeb4bedb949d7e51e
PMiranda <pmiranda.iobundle@gmail.com>
2025-08-22 17:32:01 +0100

    feat(lint): lint hardware/src FUs - remove unused signal - update signal widths to match and to remove unused bits

--- hardware/src/iob_fp_round.v
+++ hardware/src/iob_fp_round.v
@@ -11,21 +11,21 @@ module iob_fp_round #(
    input [DATA_W+3-1:0] mantissa_i,
 
    output [EXP_W-1:0]   exponent_rnd_o,
-   output [DATA_W-1:0]  mantissa_rnd_o
+   output [DATA_W-2:0]  mantissa_rnd_o
    );
 
    // Round
    wire                 round = ~mantissa_i[2]? 1'b0:
                                 ~|mantissa_i[1:0] & ~mantissa_i[3]? 1'b0: 1'b1;
 
-   wire [DATA_W-1:0]    mantissa_rnd_int = round? mantissa_i[DATA_W+3-1:3] + 1'b1: mantissa_i[DATA_W+3-1:3];
+   wire [DATA_W-2:0]    mantissa_rnd_int = round? mantissa_i[DATA_W+3-2:3] + 1'b1: mantissa_i[DATA_W+3-2:3];
 
    wire allOnes = &mantissa_i[DATA_W+3-1:3];
 
    // Normalize
-   wire [$clog2(DATA_W)-1:0] lzc;
+   wire [$clog2(DATA_W-1)-1:0] lzc;
    iob_fp_clz #(
-         .DATA_W(DATA_W)
+         .DATA_W(DATA_W-1)
          )
    clz0
      (
