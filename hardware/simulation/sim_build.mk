ae52eed055010c67089d8c7ee9539c218b841f38
PMiranda <pmiranda.iobundle@gmail.com>
2025-09-22 12:01:44 +0100

    feat(coverage): exclude tb sources from coverage - update custom coverage command to exclude modules outside `versat_ai`

--- hardware/simulation/sim_build.mk
+++ hardware/simulation/sim_build.mk
@@ -28,4 +28,13 @@ VFLAGS+=--trace-fst
 endif
 
 CUSTOM_COVERAGE_FLAGS=cov_annotated
+CUSTOM_COVERAGE_FLAGS+=-E iob_uut.v
+CUSTOM_COVERAGE_FLAGS+=-E versat_ai_mwrap.v
+CUSTOM_COVERAGE_FLAGS+=-E iob_rom_sp.v
+CUSTOM_COVERAGE_FLAGS+=-E iob_uart.v
+CUSTOM_COVERAGE_FLAGS+=-E iob_uart_core.v
+CUSTOM_COVERAGE_FLAGS+=-E iob_uart_csrs.v
+CUSTOM_COVERAGE_FLAGS+=-E iob_axi_ram.v
+CUSTOM_COVERAGE_FLAGS+=-E iob_ram_t2p_be.v
+CUSTOM_COVERAGE_FLAGS+=-E iob_ram_t2p.v
 CUSTOM_COVERAGE_FLAGS+=-o versat_ai_coverage.rpt
