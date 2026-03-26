b33a7f1622eac7c04b6f88e54d391a8b1f4f04eb
Artur Nóbrega <arturnobrega1999@gmail.com>
2026-03-26 17:27:29 +0000

    feat(python_env): Upgrade onnxruntime version, since older ones are not available (in arch linux)

--- scripts/makePythonEnv.sh
+++ scripts/makePythonEnv.sh
@@ -4,4 +4,4 @@ mkdir ../python_env
 pushd ../python_env
 python3 -m "venv" .
 source ./bin/activate
-pip install onnx==1.17.0 skl2onnx==1.17.0 matplotlib==3.10.3 onnxruntime==1.22.0 pydot==4.0.1 pulp==3.2.1
+pip install onnx==1.17.0 skl2onnx==1.17.0 matplotlib==3.10.3 onnxruntime==1.24.4 pydot==4.0.1 pulp==3.2.1
