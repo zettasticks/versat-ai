098047ec27089b59a8d22beb5f848bd41f029838
Artur Nóbrega <arturnobrega1999@gmail.com>
2026-04-01 15:47:34 +0100

    wip(tester): Add SUT initialziation sequence
    Still stuck in SUT initialization.

--- submodules/iob_system_tester/software/src/iob_system_tester_firmware.c
+++ submodules/iob_system_tester/software/src/iob_system_tester_firmware.c
@@ -15,6 +15,8 @@
 #include "iob_eth.h"
 #endif
 
+#include "iob_regfileif_csrs.h"
+
 // Enable debug messages.
 #define DEBUG 0
 
@@ -79,21 +81,44 @@ int main() {
   // test puts
   uart_puts("\n\n\nHello world from Tester!\n\n\n");
 
-#ifdef IOB_SYSTEM_TESTER_USE_ETHERNET
-  // Receive data from console via Ethernet
-  file_size = uart_recvfile_ethernet(
-      "xcelium_cov.tcl"); // NOTE: random file just for demo
-  eth_rcv_file(buffer, file_size);
-  uart_puts("\nFile received from console via ethernet:\n");
-  for (i = 0; i < file_size; i++)
-    uart_putc(buffer[i]);
-#endif // IOB_SYSTEM_TESTER_USE_ETHERNET
+  // #ifdef IOB_SYSTEM_TESTER_USE_ETHERNET
+  //   // Receive data from console via Ethernet
+  //   file_size = uart_recvfile_ethernet(
+  //       "xcelium_cov.tcl"); // NOTE: random file just for demo
+  //   eth_rcv_file(buffer, file_size);
+  //   uart_puts("\nFile received from console via ethernet:\n");
+  //   for (i = 0; i < file_size; i++)
+  //     uart_putc(buffer[i]);
+  // #endif // IOB_SYSTEM_TESTER_USE_ETHERNET
 
   //
   // Init SUT
   //
 
+  // Init SUT (connected through REGFILEIF)
+  iob_regfileif_csrs_init_baseaddr(SUT_BASE);
+
+  // Place SUT into reset state
+  iob_regfileif_csrs_set_rst(1);
+
+  // Load SUT firmware into SUT's memory zone (external memory)
+#ifdef IOB_SYSTEM_TESTER_USE_ETHERNET
+  // Receive data from console via Ethernet
+  file_size =
+      uart_recvfile_ethernet("../../../software/versat_ai_firmware.bin");
+  eth_rcv_file((char *)0x40000000,
+               file_size); // Place SUT fw in external memory
+#else                      // NOT IOB_SYSTEM_TESTER_USE_ETHERNET
+  // Receive data from console via UART
+  file_size =
+      uart_recvfile("../../../software/versat_ai_firmware.bin",
+                    (char *)0x40000000); // Place SUT fw in external memory
+#endif                     // IOB_SYSTEM_TESTER_USE_ETHERNET
+
   uart_puts("[Tester]: Initializing SUT via UART...\n");
+
+  // Disable SUT reset
+  iob_regfileif_csrs_set_rst(0);
 
   // Init and switch to uart1 (connected to the SUT)
   uart_init(UART1_BASE, IOB_BSP_FREQ / IOB_BSP_BAUD);
