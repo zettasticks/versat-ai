176679c570e8d81be73456ad5d195f59dd41b9e4
z <ruben.teixeira@tecnico.ulisboa.pt>
2026-04-15 10:07:01 +0100

    Updated python env script to fix a mistake

--- scripts/makePythonEnv.sh
+++ scripts/makePythonEnv.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-majorV="$(python -c 'import sys; print(".".join(map(str, sys.version_info[0:1])))')"
-minorV="$(python -c 'import sys; print(".".join(map(str, sys.version_info[1:2])))')"
+minorV="$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[1:2])))')"
 
-if [[ $majorV != 3 ]]; then
-   echo "Error, we require Python3"
+if [[ -z $minorV ]]; then
+   echo "Error, we require python3 to be installed and accessible"
 else
    mkdir ../python_env
    pushd ../python_env
@@ -17,4 +16,3 @@ else
       pip install onnx==1.17.0 skl2onnx==1.17.0 matplotlib==3.10.3 onnxruntime==1.22.0 pydot==4.0.1
    fi
 fi
-
